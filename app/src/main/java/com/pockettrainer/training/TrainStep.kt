6b8c19215655370c9ab6cf0ff56418751b3ee7ee
suer781 <177437350@qq.com>
2026-06-05 00:41:34 +0800

    fix: create TrainStep enum for TrainScreen compat

---
+++ app/src/main/java/com/pockettrainer/training/TrainStep.kt
@@ -0,0 +1,9 @@
+package com.pockettrainer.training
+
+enum class TrainStep(val label: String) {
+    MODEL("Model"),
+    DATA("Data"),
+    CONFIG("Config"),
+    TRAIN("Training"),
+    DONE("Done")
+}
